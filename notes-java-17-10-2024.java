e1b623315135dab143b67e4ccc2402b4c915f287
Thomás <thocano7@ielasnieves.edu.co>
2024-10-17 12:52:49 -0500

    Add files via upload

---
+++ notes-java-17-10-2024.java
@@ -0,0 +1,51 @@
+public class Main {
+    public static void main(String[] args) {
+        // Entero
+        int integer = 19;
+        System.out.println("The value range of Integer is -2 147 483 648 to 2 147 483 647");
+        System.out.println("El Int es " + integer);
+
+        //Caracter
+        char character = 'T';
+        Character caracterObject;
+        System.out.println("The value range of Character is 0 to 65.535");
+        System.out.println("El char es " + character);
+
+        //Boolean
+        boolean booleanNumber = true;
+        System.out.println("The value range of Boolean is True or False");
+        System.out.println("EL boolean es " + booleanNumber);
+    
+        //Byte
+        byte byteNumber = -100;
+        System.out.println("The value range of Byte is -128 to 127");
+        System.out.println("El byte es "+ byteNumber);
+
+        //Short
+        short shortNumber = 1000;
+        System.out.println("The value range  of Short is -32768 to 32767");
+        System.out.println("El short es " + shortNumber);
+
+        //Decimal double
+        double doubleNumber = 12.19;
+        System.out.println("The value range  of Short is ");
+        System.out.println("El double es " + doubleNumber);
+
+        //String
+        String cadena = "Thomas";
+        System.out.println("The value range of String is  cadena de caracteres");
+        System.out.println("El String es " + cadena);
+        
+        //Float / flotante
+        float floatNumber = 19.79f;
+        System.out.println("The value range of Float is 1.4e-45 to 3.4e38 ");
+        System.out.println("El float es " + floatNumber);
+
+        //Long
+        long longNumber = 1000;
+        System.out.println("The value range of Long is -9.223.372.036.854.775.808 to 9.223.372.036.854.775.807");
+        System.out.println("El long es " + longNumber);
+        
+        Object objeto;
+    }
+}
